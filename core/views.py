ce6548f6e2e662f1cacd65dc8207cfbf99f7b1d5
Thalysson <thalysson140105@gmail.com>
2025-01-16 16:00:02 -0400

    :rage1: Foi implementado um template base, o layout e um template index, impletado os comandos por bloco, configuração de arquivos static e tentando fazer o deploy

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse # Usado para retornar uma resposta HTTP
 #View baseada em função (tem como ser baseada em classe)
 def index(request):
     # return HttpResponse("Hello, world! - Index")
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'titulo':'Olá mundo com variável \"global\"'})
 
 def ola(request):
-    return HttpResponse("Olá Django!")
+    return HttpResponse("Olá Django! (SOU A VIEW VIA URL)")
